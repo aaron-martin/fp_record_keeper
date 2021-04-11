d18b35d467d798c4897b0822f6b9a5604d4760ae
Aaron Martin <Aaron@Aarons-MBP.woodhulltel.com>
2021-04-11 13:57:01 -0500

    first commit

---
+++ src/main/java/com/aaronmartin/fp_record_keeper/FpRecordKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.aaronmartin.fp_record_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpRecordKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpRecordKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaronmartin/fp_record_keeper/FpRecordKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronmartin.fp_record_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpRecordKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
